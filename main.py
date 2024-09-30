b4204a81687ef9a313a5bf1f01353559b311b03d
Julius Willrich <julius.willrich@uni-jena.de>
2024-09-30 10:38:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(5, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
